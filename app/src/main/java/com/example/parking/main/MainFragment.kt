019a5d1cf7c7e7a1a00cb07281773fa02ebb863c
hongren <id40123@gmail.com>
2023-03-14 13:36:01 +0800

    revert to base main

--- app/src/main/java/com/example/parking/main/MainFragment.kt
+++ app/src/main/java/com/example/parking/main/MainFragment.kt
@@ -4,34 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.example.parking.R
 import com.example.parking.api.data.LOGIN_001_Rq
 import com.example.parking.api.data.LOGIN_001_Rs
-import com.example.parking.ui.MainScreen
+import com.example.parking.databinding.FragmentMainBinding
+import com.example.parking.fragment.BaseViewBindingFragment
 import com.example.parking.utils.DialogUtils
+import com.example.parking.widget.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment : Fragment() {
+class MainFragment : BaseViewBindingFragment<FragmentMainBinding>() {
 
     private val viewModel: MainViewModel by viewModels()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                MainScreen { view ->
-                    { user, pwd ->
-                        viewModel.getServiceStateList(LOGIN_001_Rq(user, pwd), view)
-                    }
-                }
-            }
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setView()
@@ -40,9 +27,8 @@ class MainFragment : Fragment() {
     private fun setView() {
         viewModel.apply {
             userLiveData.observe(viewLifecycleOwner) {
-                val nav = findNavController()
                 it?.let {
-                    navigateToEntry(it, nav)
+                    navigateToEntry(it, binding.root)
                     clearResponse()
                 }
             }
@@ -53,11 +39,31 @@ class MainFragment : Fragment() {
                 }
             }
         }
+        binding.apply {
+            mbtnLogin.setOnClickListener {
+                viewModel.getServiceStateList(LOGIN_001_Rq(tilUser.text, tilPwd.text), it)
+            }
+            tilPwd.setEndIconOnClickListener(object : TextInputLayout.EndIconOnClickCallback {
+                override fun onClick(til: TextInputLayout, view: View) {
+                    val inputTypeStateSwitchTo = when (til.getInputType()) {
+                        TextInputLayout.INPUT_TYPE_TEXT_PASSWORD_MASK -> {
+                            Pair(TextInputLayout.INPUT_TYPE_TEXT_PASSWORD, R.drawable.ic_eye_open_apple)
+                        }
+                        else -> Pair(TextInputLayout.INPUT_TYPE_TEXT_PASSWORD_MASK, R.drawable.ic_eye_close_apple)
+                    }
+                    val inputType = til.getTypeface() //保持隱碼為全型，避免被覆蓋
+                    til.setInputType(inputTypeStateSwitchTo.first)
+                    til.setCustomIcon(inputTypeStateSwitchTo.second)
+                    til.putCursorToTextTail()
+                    til.setTypeface(inputType)
+                }
+            })
+        }
     }
 
-    private fun navigateToEntry(result: LOGIN_001_Rs?, navController: NavController) {
+    private fun navigateToEntry(result: LOGIN_001_Rs?, view: View) {
         val direction = MainFragmentDirections.actionToEntry(result)
-        navController.navigate(direction)
+        view.findNavController().navigate(direction)
     }
 
     private fun onServiceAPIError() {
@@ -67,5 +73,9 @@ class MainFragment : Fragment() {
             msg = resources.getString(R.string.common_login_failure),
             rightButtonText = resources.getString(R.string.common_text_i_know_it),
         )
+    }
+
+    override fun bindingCallback(): (LayoutInflater, ViewGroup?) -> FragmentMainBinding = { layoutInflater, viewGroup ->
+        FragmentMainBinding.inflate(layoutInflater, viewGroup, false)
     }
 }
